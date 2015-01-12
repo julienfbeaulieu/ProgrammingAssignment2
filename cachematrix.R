0a659209bfbf61bb0ffc29fa71d5b4f4337f342c
julienfbeaulieu <julienfbeaulieu@gmail.com>
2015-01-12 15:20:29 -0500

    Update comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,11 @@
+##makeCacheMatrix and cacheSolve is a set of two functions allowing
+##to calculate the inverse of a square matrix and cache the result 
+##for later use
+
+##makeCacheMatrix expands the atomic vector x into a list array
+##containing its value and the value of its inverse. Initially,
+##the inverse 's' is set to NULL.
+
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
   set <- function(y) {
@@ -14,6 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
+##cacheSolve expands on the the 'solve' function to add a cache 
+## component. When called the first time, it returns the inverse
+## of x and stores the answer with 'setSolve'. On a second call,
+## the function simply returns the cached value.
 
 cacheSolve <- function(x, ...) {
   s <- x$getSolve()
